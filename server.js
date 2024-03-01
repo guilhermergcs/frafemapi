8c3bbbb2d641df6893f7645138ac42bab1b7078a
guilhermergcs <guilhermergcs@outlook.com>
2024-03-01 12:19:37 -0300

    Atualização server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 
 const app = express();
 app.use(cors());
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Configuração do MySQL
 const connection = mysql.createConnection({
